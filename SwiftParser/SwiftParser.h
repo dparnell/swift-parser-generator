dcb548f368e5cee75c221e95073c141ece9d0c9f
Daniel Parnell <me@danielparnell.com>
2014-06-17 18:51:53 +1000

    Initial Commit

---
+++ SwiftParser/SwiftParser.h
@@ -0,0 +1,19 @@
+//
+//  SwiftParser.h
+//  SwiftParser
+//
+//  Created by Daniel Parnell on 17/06/2014.
+//  Copyright (c) 2014 Daniel Parnell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftParser.
+FOUNDATION_EXPORT double SwiftParserVersionNumber;
+
+//! Project version string for SwiftParser.
+FOUNDATION_EXPORT const unsigned char SwiftParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftParser/PublicHeader.h>
+
+
